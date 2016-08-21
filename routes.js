a9f0d7d31c1ca8f353069c867299c2f0cf1ba0c1
Pittayarat <pittayarat.sae@gmail.com>
2016-08-21 22:55:11 +0700

    initial project

---
+++ routes.js
@@ -0,0 +1,28 @@
+1
+"use strict";
+import express from 'express'
+import bodyParser from 'body-parser'
+import facebookRouter from './controller/FacebookController'
+
+
+const port = process.env.port || 7777
+let app = express()
+
+
+// parse application/json
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+
+//parent url
+app.route('/').get((req, res) => {
+  res.send('<h1>Social REST Api</h1><ul><li>/twitter</li><li>/facebook</li></ul>')
+})
+
+app.use('/facebook', facebookRouter)
+
+//service start
+app.listen(port, () => {
+  console.log('Starting node.js on port ' + port)
+});
---
+++ service/index.js
@@ -0,0 +1,1 @@
+export * as FacebookService from "./FacebookService";
